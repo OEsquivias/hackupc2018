e3b6bf1f991a65cc99a5304176edbe2b39ad1535
Wellinstein <32274311+Wellinstein@users.noreply.github.com>
2018-10-20 15:25:48 +0200

    Update webpack/app/scripts/app.js

--- webpack/app/scripts/app.js
+++ webpack/app/scripts/app.js
@@ -49,14 +49,18 @@ const App = {
       gpsLon.innerHTML = parseInt(gps[1])*1./1e6;
     });
   },
+
   makeTweet: function(){
+    const lon = document.getElementById('lon').value
+    const lat = document.getElementById('lat').value
+
       var Twit = require('twit')
 
       var T = new Twit({
         consumer_key:         '...',
-        consumer_secret:      '...',
+        // consumer_secret:      '...',
         access_token:         '...',
-        access_token_secret:  '...',
+        // access_token_secret:  '...',
         timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
         strictSSL:            true,     // optional - requires SSL certificates to be valid.
       })
@@ -64,7 +68,7 @@ const App = {
       //
       //  tweet 'hello world!'
       //
-      T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+      T.post('statuses/update', { status: 'Possible crime in coordinates (' +   }, function(err, data, response) {
         console.log(data)
       })
 }
