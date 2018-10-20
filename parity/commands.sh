79df1b4faa85b92cd617323c6177b95dafb550ba
alfon994 <alfonso-16-9-94@hotmail.com>
2018-10-20 19:05:42 +0200

    New version

---
+++ parity/commands.sh
@@ -0,0 +1,4 @@
+curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node0", "node0"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8540
+curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["user", "user"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8540
+curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["node1", "node1"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
+parity --config node0.toml
