e13c82a3ad9a70a960ee7df32e94ff3ac94cadc2
alfon994 <alfonso-16-9-94@hotmail.com>
2018-10-20 12:15:56 +0200

    Initial

---
+++ webpack/app/scripts/app.js
@@ -0,0 +1,66 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import '../styles/app.css'
+
+// Import libraries we need.
+import { default as Web3 } from 'web3'
+import { default as contract } from 'truffle-contract'
+
+// Import our contract artifacts and turn them into usable abstractions.
+import INeedAHeroArtifact from '../../build/contracts/INeedAHero.json'
+
+// MetaCoin is our usable abstraction, which we'll use through the code below.
+const INeedAHero = contract(INeedAHeroArtifact)
+
+let account;
+
+const App = {
+  start: function () {
+    const self = this
+
+    // Bootstrap the MetaCoin abstraction for Use.
+    INeedAHero.setProvider(web3.currentProvider)
+
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts(function (err, accs) {
+      account = accs[0]
+    })
+  },
+
+  setGps: function () {
+
+    const lon = parseInt(document.getElementById('lon').value*10^6)
+    const lat = parseInt(document.getElementById('lat').value*10^6)
+
+    INeedAHero.deployed().then(function (instance) {
+      instance.setGps(account, lat, lon, {from:account});
+    });
+  },
+
+  getGps: function () {
+
+    var gpsLon = document.getElementById('gpsLon')
+    var gpsLat = document.getElementById('gpsLat')
+
+    INeedAHero.deployed().then(function (instance) {
+      instance.getGps(account, {from:account}).then(function (lon, lat){
+        gpsLon.innerHtml = lon*1./10^6;
+        gpsLon.innerHtml = lat*1./10^6;
+      });
+    });
+  }
+}
+
+window.App = App
+
+window.addEventListener('load', function () {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider)
+  } else {
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+  }
+
+  App.start()
+})
---
+++ webpack/contracts/INeedAHero.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.20;
+
+contract INeedAHero {
+    struct person {
+        bytes32 id;
+        address account;
+        int lat;
+        int lon;
+    }
+
+    mapping (bytes32 => person) public people;
+    mapping (bytes32 => mapping (bytes32 => bool)) public matches;
+
+    function setGps(bytes32 _id, int _lat, int _lon) public {
+        people[_id].account = msg.sender;
+        people[_id].id = _id;
+        people[_id].lat = _lat;
+        people[_id].lon = _lon;
+    }
+
+    function getGps(bytes32 _id) public view returns(int _lat, int _lon) {
+        _lat = people[_id].lat;
+        _lon = people[_id].lon;
+    }
+
+    function changeMatch (bytes32 _id1, bytes32 _id2) public {
+        if(matches[_id1][_id2]) {
+            matches[_id1][_id2] = false;
+        }
+        else{
+            matches[_id1][_id2] = true;
+        }
+    }
+}
